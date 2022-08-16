7fd64c782e289af14cb5407353731341123f4369
Roman Timoshkov <timoshkow@mail.ru>
2022-08-16 16:45:59 +0300

    implementation of the task on the topic 'Functions'

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,61 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
+  max = arr[0];
+  min = arr[0];
+  sum = 0;
 
-  // Ваш код
-
+  for (let arg of arr) {
+    if (min > arg) {
+      min = arg;
+    }
+    if (max < arg) {
+      max = arg;
+    }
+    sum += arg;
+    avg = Number((sum / arr.length).toFixed(2));
+  }
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
-  // Ваш код
+  for (let arg of arr) {
+    sum += arg;
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = 0;
 
-  // Ваш кода
-  // for ...
+  for (let arr of arrOfArr) {
+    let sum = func(arr);
+    if (max < sum) {
+      max = sum;
+    }
+  }
   
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min = arr[0];
+  let max = arr[0];
+  let result = 0;
+
+  for (let arg of arr) {
+    if (min > arg) {
+      min = arg
+    }
+    if (max < arg) {
+      max = arg;
+    }
+    result = Math.abs(min - max);
+  }
+  return result;
 }
