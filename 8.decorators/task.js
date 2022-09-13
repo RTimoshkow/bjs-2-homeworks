d79cbc019128f054c4d8a9913b0f115aa7d74524
Roman Timoshkov <timoshkow@mail.ru>
2022-09-13 15:14:08 +0300

    decorators, solving tasks #1 and #2

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,46 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+  function wrapper(...args) {
+    const hach = func(...args);
+    let objectInCache = cache.find(item => item === hach);
+    if(objectInCache) {
+      console.log("Из кэша: " + objectInCache);
+      return "Из кэша: " + objectInCache;
+    }
+    const result = func(...args);
+    cache.push(result);
+    if(cache.length > 5) {
+      cache.shift();
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
+  return wrapper;
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, delay) {
+  let timeoutId = null;
+  wrapper.allCount = 0;
+  wrapper.count = 0;
+  function wrapper(...args) {
+
+    if(timeoutId === null) {
+      func(...args);
+      wrapper.count++;
+    }
+    
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      func(...args)
+      wrapper.count++;
+    }, delay);
+    wrapper.allCount++;
+  }
+
+  console.log(wrapper.count);
+  console.log(wrapper.allCount);
+  
+  return wrapper;
 }
